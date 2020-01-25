6f1d38b16fdb113632e144ad291674c4aba0fb94
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-25 21:14:03 +0800

    Added StateType

---
+++ game/StateType.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+using StateInt = int;
+
+/**
+ * Типы состояний игры
+ * 
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class StateType
+{
+public:
+    /**
+     * Игра не инициализированна
+     **/
+    static const StateInt NONE = -1;
+    /**
+     * Игрок находится в главном меню
+     **/
+    static const StateInt MAIN_MENU = 1;
+    /**
+     * Игрок находится в меню настроек
+     **/
+    static const StateInt SETTINGS_MENU = 2;
+    /**
+     * Игрок находится в игре
+     **/
+    static const StateInt GAME = 3;
+};
