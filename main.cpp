fb7deab590a9e73fd4b11ad854bfb65725e7e790
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-26 20:07:12 +0800

    Update entry point

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@
 #include "fileSystem/Settings.hpp"
 
 #include "menu/MainMenu.hpp"
+#include "menu/Nav.hpp"
+#include "content/MainMenuContent.hpp"
 
 int main()
 {
     Settings settings;
-    MainMenu mainMenu("assets/img/menu/main-background.png", settings);    
+    MainMenuContent content;
+    
+    Nav nav(
+        content.get(settings.getLanguage()),
+        "assets/font/RussoOne-Regular.ttf"
+    );
+
+    MainMenu mainMenu("assets/img/menu/main-background.png", &nav);
 
     (Game(settings)).run(mainMenu);
-    
+
     return 0;
 }
