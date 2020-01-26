3c2310fc5a4dcf4219ae18d259f10fe844077ab7
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-26 18:49:07 +0800

    Added Nav

---
+++ menu/Nav.cpp
@@ -0,0 +1,45 @@
+#include "Nav.hpp"
+
+Nav::Nav(std::vector<std::string> titles, std::string pathToFont)
+{
+    float offsetWidth = (Settings()).getWidthWindow() / 2.5;
+    float offsetHeight = (Settings()).getHeightWindow() / 3;
+    const float stepHeight = (Settings()).getHeightWindow() / 10;
+
+    for(const auto& title : titles)
+    {
+        this->buttons.push_back(
+            std::shared_ptr<Button>(
+                new Button(
+                    title,
+                    pathToFont,
+                    30,
+                    sf::Vector2f(offsetWidth, offsetHeight
+                )
+            )
+        ));
+        offsetHeight += stepHeight;
+    }
+
+    this->indexOfActiveButton = 0;
+    this->buttons[this->indexOfActiveButton]->toggleActive();
+}
+
+u_int32_t Nav::getActive()
+{
+    return this->indexOfActiveButton;
+}
+
+void Nav::setActive(u_int32_t index)
+{
+    if(index < 0 || this->buttons.size() >= index) return;
+
+    this->buttons[this->indexOfActiveButton]->toggleActive();
+    this->indexOfActiveButton = index;
+    this->buttons[this->indexOfActiveButton]->toggleActive();
+}
+
+std::vector<std::shared_ptr<Button>> Nav::getButtons()
+{
+    return this->buttons;
+}
---
+++ menu/Nav.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <vector>
+#include <memory>
+#include <string>
+#include <sys/types.h>
+
+#include "Button.hpp"
+#include "../fileSystem/Settings.hpp"
+
+/**
+ * Данный класс выражает сущность меню
+ * 
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class Nav
+{
+private:
+    /**
+     * Кнопки
+     **/
+    std::vector<std::shared_ptr<Button>> buttons;
+    /**
+     * Индекс активной кнопки
+     **/
+    u_int32_t indexOfActiveButton;
+public:
+    Nav(std::vector<std::string> titles, std::string pathToFont);
+
+    u_int32_t getActive();
+    void setActive(u_int32_t index);
+
+    std::vector<std::shared_ptr<Button>> getButtons();
+};
