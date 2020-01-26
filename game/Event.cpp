45f2f98ef2a4f723e3efd8004eb9728b481a9c9d
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-26 13:48:14 +0800

    Refactoring

--- game/Event.cpp
+++
@@ -1,9 +0,0 @@
-#include "Event.hpp"
-
-/**
- * Обработка событий
- **/
-void Event::handle(sf::RenderWindow& window, sf::Event& event)
-{
-    if(event.type == sf::Event::Closed) window.close();
-}
