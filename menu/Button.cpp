035f833201d91e40a33da5e1ac3660549b999306
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-26 18:48:54 +0800

    Added Button

---
+++ menu/Button.cpp
@@ -0,0 +1,31 @@
+#include "Button.hpp"
+
+Button::Button(std::string title, std::string pathToFont, u_int32_t sizeOfTitle, sf::Vector2f position)
+{
+    this->title = title;
+    this->position = position;
+    this->fontOfTitle.loadFromFile(pathToFont);
+    this->button.setPosition(this->position);
+    this->button.setFont(this->fontOfTitle);
+    this->button.setCharacterSize(sizeOfTitle);
+    this->button.setString(this->title);
+    this->button.setFillColor(sf::Color::White);
+    this->active = false;
+}
+
+void Button::toggleActive()
+{
+    if(this->active)
+    {
+        this->active = false;
+        this->button.setFillColor(sf::Color::White);
+        return;
+    }
+    this->active = true;
+    this->button.setFillColor(sf::Color::Blue);
+}
+
+sf::Drawable& Button::show()
+{
+    return this->button;
+}
---
+++ menu/Button.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <sys/types.h>
+
+#include "../game/state/StateType.hpp"
+#include "../game/IShow.hpp"
+
+/**
+ * Сущность кнокпи
+ * 
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class Button : public IShow
+{
+private:
+    /**
+     * Заголовок кнопки
+     **/
+    std::string title;
+    /**
+     * Ссылка на новое состояние
+     **/
+    StateInt link;
+    /**
+     * Шрифт заголовка
+     **/
+    sf::Font fontOfTitle;
+    /**
+     * Позиция кнопки
+     **/
+    sf::Vector2f position;
+    /**
+     * Кнопка
+     **/
+    sf::Text button;
+    /**
+     * Указывает на то, является ли кнопка активной
+     **/
+    bool active;
+public:
+    Button(std::string title, std::string pathToFont, u_int32_t sizeOfTitle, sf::Vector2f position);
+    void toggleActive();
+    sf::Drawable& show() override;
+};
