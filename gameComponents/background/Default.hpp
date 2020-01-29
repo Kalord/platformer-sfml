ee4427f3280e46d7b229919101cd37a5ea789a2c
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-30 01:38:33 +0800

    Added Default background

---
+++ gameComponents/background/Default.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Background.hpp"
+
+/**
+ * Стандартный игровой фон для большинства уровней
+ * 
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class Default : public Background
+{
+public:
+    Default();
+};
