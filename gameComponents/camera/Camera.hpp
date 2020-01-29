2677f6e628160968339122a2a68d4b8af7034eb2
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-30 01:38:52 +0800

    Added Camera

---
+++ gameComponents/camera/Camera.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../IComponent.hpp"
+
+/**
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class Camera : public IComponent
+{
+public:
+    virtual ~Camera() {}
+};
