23c540f16ace8dbf55d11c2bae457a633887a7c0
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-02-02 23:28:34 +0800

    Added logic for work with camera

--- gameComponents/camera/Camera.hpp
+++ gameComponents/camera/Camera.hpp
@@ -1,12 +1,41 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
+#include <memory>
+
 #include "../IComponent.hpp"
+#include "../tileMap/TileMap.hpp"
+#include "../TileContainer.hpp"
 
 /**
+ * Игровая камера
+ * 
+ * Данная камера являются viewport'ом для сцены
+ * 
  * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
  **/
 class Camera : public IComponent
 {
+private:
+    /**
+     * Текущие положение камеры
+     **/
+    sf::Vector2i center;
+    /**
+     * Сдвиг от центра камеры, зона видимости
+     **/
+    const sf::Vector2i offset{5, 2};
 public:
+    Camera(int x, int y);
     virtual ~Camera() {}
+
+    /**
+     * Обновление viewport'а игры
+     **/
+    void update(
+        sf::RenderWindow& window,
+        std::shared_ptr<TileMap> tileMap,
+        std::shared_ptr<TileContainer> tileContainer
+    );
 };
