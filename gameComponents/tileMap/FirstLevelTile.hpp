25f372dcf799c27ff2a066a5b66087cde2ac0f43
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-02-02 23:26:25 +0800

    Added FirstLevelTile

---
+++ gameComponents/tileMap/FirstLevelTile.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "TileMap.hpp"
+
+/**
+ * Тайловая карта для первого уровня
+ * 
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class FirstLevelTile : public TileMap
+{
+public:
+    FirstLevelTile();
+};
