936849216c65a2f03c14712cc5fe030c170b9881
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-02-02 23:26:06 +0800

    Added Camera

---
+++ gameComponents/camera/Camera.cpp
@@ -0,0 +1,45 @@
+#include "Camera.hpp"
+
+Camera::Camera(int x, int y) : center({x, y})
+{
+}
+
+/**
+ * Обновление viewport'а игры
+ **/
+void Camera::update(
+    sf::RenderWindow& window,
+    std::shared_ptr<TileMap> tileMap,
+    std::shared_ptr<TileContainer> tileContainer
+)
+{
+    int i = this->center.y - this->offset.y;
+    sf::Vector2f tilePosition(0.0f, 0.0f);
+    const float tileOffset = 32.0f;
+
+    for(i; i < this->center.y + this->offset.y; i++)
+    {
+        int j = this->center.x - this->offset.x;
+        for(j; j < this->center.x + this->offset.x; j++)
+        {
+            char identityTile = tileMap->getTile(j, i);
+
+            //@tmp
+            if(identityTile == '0')
+            {
+                tilePosition.x += tileOffset;
+                continue;
+            }
+            //@tmp
+
+            sf::Sprite tile = tileContainer->getTileByIdentity(identityTile);
+            tile.setPosition(tilePosition);
+            window.draw(tile);
+
+            tilePosition.x += tileOffset;
+        }
+
+        tilePosition.y += tileOffset;
+        tilePosition.x = 0;
+    }
+}
